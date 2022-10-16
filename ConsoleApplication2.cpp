ef4912a7e8d365a124611bc2a447102d8856aa03
FelixVoro <115408780+FelixVoro@users.noreply.github.com>
2022-10-16 16:55:37 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int a, f, s, f2, s2;
+	cin >> a;
+	f = a / 1000;
+	s = a % 1000;
+	f2 = f/100 +f/10%10+f%10;
+	s2 = s / 100 + s / 10 % 10 + s % 10;
+	if (f2 == s2)
+		cout << "YES";
+	else
+		cout << "NO";
+
+}
